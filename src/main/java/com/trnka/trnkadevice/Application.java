a7ad5b2afb359a1ec2f0d78a9214b5171a3c1cf6
tgramblicka <t.gramblicka@gmail.com>
2019-08-12 00:51:50 +0200

    exclude of autoconfiguration, problem with datasource on springBoot startup: EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})

--- src/main/java/com/trnka/trnkadevice/Application.java
+++ src/main/java/com/trnka/trnkadevice/Application.java
@@ -1,11 +1,19 @@
 package com.trnka.trnkadevice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.trnka.trnkadevice")
 @Slf4j
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaRepositories()
 public class Application {
 
 	public static void main(String[] args) {
