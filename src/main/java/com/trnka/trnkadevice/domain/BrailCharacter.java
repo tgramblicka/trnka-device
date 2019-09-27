ddd52aeab134ac285dc8c4c7b13b3f41d7e6d994
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-09-27 16:11:10 +0200

    UserType dependency + brailCharacter domain - sequence will be integer array

--- src/main/java/com/trnka/trnkadevice/domain/BrailCharacter.java
+++ src/main/java/com/trnka/trnkadevice/domain/BrailCharacter.java
@@ -1,14 +1,27 @@
 package com.trnka.trnkadevice.domain;
 
-import com.trnka.trnkadevice.inputreader.Keystroke;
-import com.trnka.trnkadevice.ui.messages.IMessage;
-
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.trnka.trnkadevice.inputreader.Keystroke;
+import com.trnka.trnkadevice.ui.messages.IMessage;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "brail_character")
 public class BrailCharacter implements IMessage {
+
+    @Id
     private Long id;
     private Character character;
-    private List<Keystroke> brailRepresentation;
+    private List<Keystroke> brailRepresentationKeystrokes;
+    @Column(name = "brail_representation", columnDefinition = "json")
+    @Type(type = "json")
+    private List<Integer> brailRepresentation;
     private String audioFile;
 
     public BrailCharacter() {
@@ -31,12 +44,12 @@ public class BrailCharacter implements IMessage {
         this.character = character;
     }
 
-    public List<Keystroke> getBrailRepresentation() {
-        return brailRepresentation;
+    public List<Keystroke> getBrailRepresentationKeystrokes() {
+        return brailRepresentationKeystrokes;
     }
 
-    public void setBrailRepresentation(final List<Keystroke> brailRepresentation) {
-        this.brailRepresentation = brailRepresentation;
+    public void setBrailRepresentationKeystrokes(final List<Keystroke> brailRepresentation) {
+        this.brailRepresentationKeystrokes = brailRepresentation;
     }
 
     public void setAudioFile(final String audioFile) {
