858ef983a1db75911e19c907cbee9b233c08f23a
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-10-21 08:55:05 +0200

    InitialData insertion test class

---
+++ src/test/java/com/trnka/trnkadevice/InsertInitialData.java
@@ -0,0 +1,42 @@
+package com.trnka.trnkadevice;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trnka.trnkadevice.repository.BrailCharacterRepository;
+import com.trnka.trnkadevice.repository.LearningSequenceRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+@ActiveProfiles("dev")
+@Transactional
+public class InsertInitialData {
+
+    @Autowired
+    private LearningSequenceRepository repo;
+    @Autowired
+    BrailCharacterRepository brailCharacterRepository;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    @Rollback(false)
+    public void fillDB() {
+        MockBrails.BRAIL.forEach(brail -> brailCharacterRepository.save(brail));
+
+        // MockData.SEQUENCES.forEach(sequence -> {
+        // repo.save(sequence);
+        // });
+    }
+
+}
