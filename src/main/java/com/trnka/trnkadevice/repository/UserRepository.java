af833ae2d06f8d79b0f38b919a0c644ed4c3cbbe
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-10-21 17:33:43 +0200

    UserRepository

---
+++ src/main/java/com/trnka/trnkadevice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.trnka.trnkadevice.repository;
+
+import com.trnka.trnkadevice.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
