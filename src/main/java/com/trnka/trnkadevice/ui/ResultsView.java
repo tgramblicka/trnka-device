c5bba8ec722f4d7665843c220a183e95d18495ab
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-10-27 21:36:26 +0100

    Results and Testing -> functionality to get back to MainMenu

--- src/main/java/com/trnka/trnkadevice/ui/ResultsView.java
+++ src/main/java/com/trnka/trnkadevice/ui/ResultsView.java
@@ -1,10 +1,10 @@
 package com.trnka.trnkadevice.ui;
 
-import com.trnka.trnkadevice.ui.navigation.Navigator;
-import com.trnka.trnkadevice.ui.navigation.NavigatorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.trnka.trnkadevice.inputreader.InputReader;
+import com.trnka.trnkadevice.inputreader.Keystroke;
 import com.trnka.trnkadevice.renderer.IRenderer;
 import com.trnka.trnkadevice.ui.messages.Messages;
 
@@ -12,20 +12,22 @@ import com.trnka.trnkadevice.ui.messages.Messages;
 public class ResultsView implements IView {
 
     private IRenderer renderer;
-    private Navigator navigator;
+    private InputReader inputReader;
 
     @Autowired
     public ResultsView(final IRenderer renderer,
-                       Navigator navigator) {
+                       final InputReader inputReader) {
         this.renderer = renderer;
-        this.navigator = navigator;
+        this.inputReader = inputReader;
     }
 
     @Override
     public void enter() {
         renderer.renderMessage(Messages.RESULTS_VIEW);
-
-        NavigatorUtil.registerMainMenuNavigation(navigator);
+        Keystroke key = inputReader.readFromInput();
+        while (key != null) {
+            key = inputReader.readFromInput();
+        }
     }
 
     @Override
--- src/main/java/com/trnka/trnkadevice/ui/TestingView.java
+++ src/main/java/com/trnka/trnkadevice/ui/TestingView.java
@@ -1,33 +1,38 @@
 package com.trnka.trnkadevice.ui;
 
-import com.trnka.trnkadevice.ui.navigation.Navigator;
-import com.trnka.trnkadevice.ui.navigation.NavigatorUtil;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.trnka.trnkadevice.inputreader.InputReader;
+import com.trnka.trnkadevice.inputreader.Keystroke;
 import com.trnka.trnkadevice.renderer.IRenderer;
 import com.trnka.trnkadevice.ui.messages.Messages;
 
 @Component
 public class TestingView implements IView {
 
-    private Navigator navigator;
     private IRenderer renderer;
+    private InputReader inputReader;
 
     @Autowired
-    public TestingView(final Navigator navigator,
-                       final IRenderer renderer) {
-        this.navigator = navigator;
+    public TestingView(final IRenderer renderer,
+                       final InputReader inputReader) {
         this.renderer = renderer;
+        this.inputReader = inputReader;
     }
 
     @Override
     public void enter() {
         renderer.renderMessage(Messages.TESTING_VIEW);
-        NavigatorUtil.registerMainMenuNavigation(navigator);
+        Keystroke key = inputReader.readFromInput();
+        while (key != null) {
+            key = inputReader.readFromInput();
+        }
     }
 
-    @Override public Messages getLabel() {
+    @Override
+    public Messages getLabel() {
         return Messages.TESTING_LABEL;
     }
 }
