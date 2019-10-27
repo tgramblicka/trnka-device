6b151af452754e1706d39e887ed3e4c6f890ad97
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-10-27 21:39:43 +0100

    Keystroke new interrupt parameter (not yet used)

--- src/main/java/com/trnka/trnkadevice/inputreader/Keystroke.java
+++ src/main/java/com/trnka/trnkadevice/inputreader/Keystroke.java
@@ -3,24 +3,24 @@ package com.trnka.trnkadevice.inputreader;
 import java.util.EnumSet;
 
 public enum Keystroke {
-    BRAIL_KEY_1(241, "1"),
-    BRAIL_KEY_2(242, "2"),
-    BRAIL_KEY_3(243, "3"),
-    BRAIL_KEY_4(244, "4"),
-    BRAIL_KEY_5(245, "5"),
-    BRAIL_KEY_6(246, "6"),
+    BRAIL_KEY_1(241, "1", false),
+    BRAIL_KEY_2(242, "2", false),
+    BRAIL_KEY_3(243, "3", false),
+    BRAIL_KEY_4(244, "4", false),
+    BRAIL_KEY_5(245, "5", false),
+    BRAIL_KEY_6(246, "6", false),
 
-    UP(67, "up"), // add volume on device
-    DOWN(66, "down"), // add volume on device
-    SUBMIT(59, "submit"), // help on device
+    UP(67, "up", false), // add volume on device
+    DOWN(66, "down", false), // add volume on device
+    SUBMIT(59, "submit", false), // help on device
 
-    RESERVED_1(60, "reserve1"),
-    RESERVED_2(61, "reserve1"),
+    RESERVED_1(60, "reserve1", true),
+    RESERVED_2(61, "reserve1", true),
 
-    MENU_1(62, "menu1"),
-    MENU_2(63, "menu2"),
-    MENU_3(64, "menu3"),
-    MENU_4(65, "menu4"),
+    MENU_1(62, "menu1", true),
+    MENU_2(63, "menu2", true),
+    MENU_3(64, "menu3", true),
+    MENU_4(65, "menu4", true),
 
     // KEY_1(2, "1"),
     // KEY_2(3, "2"),
@@ -33,18 +33,21 @@ public enum Keystroke {
     // KEY_9(10, "9"),
     // KEY_0(11, "0"),
 
-    KEY_A(30, "a"),
-    KEY_B(45, "b"),
+    KEY_A(30, "a", false),
+    KEY_B(45, "b", false),
 
-    UNKNOWN(0, "-");
+    UNKNOWN(0, "-", false);
 
     private final Integer code;
     private final String value;
+    private final boolean interruptsFurtherProcessing;
 
     Keystroke(Integer code,
-              String value) {
+              String value,
+              boolean interruptsFurtherProcessing) {
         this.code = code;
         this.value = value;
+        this.interruptsFurtherProcessing = interruptsFurtherProcessing;
     }
 
     public Integer getCode() {
@@ -53,6 +56,10 @@ public enum Keystroke {
 
     public String getValue() {
         return value;
+    }
+
+    public boolean getInterruptsFurtherProcessing() {
+        return interruptsFurtherProcessing;
     }
 
     public static Keystroke getByCode(final Integer code) {
