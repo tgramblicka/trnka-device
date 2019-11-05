61c0feef7b165d204f200b609aadce439b8b30b2
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-11-06 00:28:06 +0100

    copy script - rsync is faster than scp liquibase-run - adaptation for raspberrry

--- copy.sh
+++ copy.sh
@@ -2,5 +2,7 @@
 
 #mvn clean compile assembly:single
 #mvn clean install
-scp ./target/trnka-device.jar pi@192.168.1.15:trnka-device/
+
+#scp ./target/trnka-device.jar pi@192.168.1.15:trnka-device/
+rsync --progress -avz target/trnka-device.jar pi@192.168.1.15:/home/pi/trnka-device
 
--- liquibase-run.sh
+++ liquibase-run.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 mariaDbClientJar="/c/Users/200000591/apps/mariadb-java-client-2.4.3.jar"
+host="jdbc:mariadb://localhost:3306/trnka-device"
+raspi_host="jdbc:mariadb://192.168.1.15:3306/trnka-device"
+pwd="root"
+raspi_pwd="raspberry"
 
+
+# LIQUIBASE FOR PC
 liquibase \
 --changeLogFile="./src/main/resources/db/changelog/changelog-master.xml" \
---url="jdbc:mariadb://localhost:3306/trnka-device" \
+--url=$raspi_host \
 --driver="org.mariadb.jdbc.Driver" \
 --classpath="$mariaDbClientJar" \
 --username="root" \
---password="root" \
+--password=$raspi_pwd \
 --logLevel="INFO" \
 update
+
+
 
 
 
