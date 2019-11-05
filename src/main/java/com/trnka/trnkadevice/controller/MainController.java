b2f075a3b681fb1a625c6fda1ac01cff84dd0342
Tomas Gramblicka <t.gramblicka@gmail.com>
2019-11-06 00:28:37 +0100

    unused import

--- src/main/java/com/trnka/trnkadevice/controller/MainController.java
+++ src/main/java/com/trnka/trnkadevice/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.trnka.trnkadevice.controller;
 
-import com.trnka.trnkadevice.ui.LoginView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
