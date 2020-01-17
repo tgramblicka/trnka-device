40c1d192ca81bfcb8d4d3d4cafd561dd4744c020
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-01-17 16:08:58 +0100

    Messages adapted

--- src/main/java/com/trnka/trnkadevice/ui/messages/Messages.java
+++ src/main/java/com/trnka/trnkadevice/ui/messages/Messages.java
@@ -1,37 +1,38 @@
 package com.trnka.trnkadevice.ui.messages;
 
+import java.util.EnumSet;
+
 public enum Messages implements IMessage {
     LOGIN_VIEW_LABEL("Prihlasenie", "audio"),
-    TYPE_IN_YOUR_PASSWORD("Zadajte 4 miestny prihlasovaci kod", "audio"),
+    TYPE_IN_YOUR_PASSWORD("Zadaj 4 miestny prihlasovaci kod a potvrd", "audio"),
     YOU_HAVE_BEEN_SUCCESSFULY_LOGGED_IN("Boli ste uspesne prihlaseny", "audio"),
     WRONG_PASSWORD("Heslo nespravne", "audio"),
-    MAIN_MENU_LABEL("Sekcia: hlavne menu Student", "audio"),
+    MAIN_MENU_LABEL("Sekcia: Hlavne menu - Student", "audio"),
     MAIN_MENU("Vitaj {}. Posun v menu tlacidlami hore/dole. Vstup do menu potvrdzovacim tlacidlom", "audio"),
-    LEARNING_LABEL("Vyuka", "audio"),
+    LEARNING_LABEL_MENU("Vyuka", "audio"),
     LEARNING_VIEW_MENU("Sekcia: Vyuka, vyber moznosti tlacidlami hore/dole", "audio"),
     METHODICAL_LEARNING_MENU("Metodicka vyuka", "audio"),
     METHODICAL_LEARNING_VIEW("Sekcia: Metodicka Vyuka, vyber moznosti tlacidlami hore/dole", "audio"),
-    TESTING_LABEL("Testovanie", "audio"),
+    TESTING_LABEL_MENU("Testovanie", "audio"),
     TESTING_VIEW("Sekcia: Testovanie. Vyber test tlacidlami hore/dole", "audio"),
 
-    RESULTS_LABEL("Vysledky", "audio"),
+    RESULTS_LABEL_MENU("Vysledky", "audio"),
     RESULTS_SELECTION_VIEW("Sekcia Vysledky: Vyber vysledok testu zo zoznamu tlacidlami hore/dole", "audio"),
-    RESULTS_VIEW("Sekcia: Vyuka, vyber moznosti tlacidlami hore/dole", "audio"),
+    RESULTS_VIEW("Vysledky pre vybrany test", "audio"),
     RESULT_NO_RESULTS_TO_DISPLAY("Nenasli sa ziadne Vase vysledky", "audio"),
 
-    LEARNING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre {} a potvrd!", "audio"),
+    LEARNING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre '{}' a potvrd!", "audio"),
     LEARNING_CORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("Spravne, pokracujem na dalsie pismeno", "audio"),
-    LEARNING_INCORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("NESPRAVNE! Spravna sekvencia je: {} .Ostava {} pokusov.", "audio"),
-    LEARNING_MAXIMUM_NEGATIVE_TRIES_REACHED("Maximalny pocet nespravnych opakovani bol dosiahnuty!", "audio"),
+    LEARNING_INCORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("NESPRAVNE! Spravna sekvencia je: '{}' .Ostava {} pokusov.", "audio"),
     LEARNING_SEQUENCE_END("Koniec uciacej sekvencie!", "audio"),
 
 
-    METHODICAL_LEARNING_TEST_ENTERED("Zacina sa ostry test", "audio"),
-    METHODICAL_LEARNING_ENDED("Koniec testu!", "audio"),
-    METHODICAL_LEARNING_TEST_PASSED("Gratulujem! Uspesne ste presli testom!", "audio"),
-    METHODICAL_LEARNING_TEST_NOT_PASSED("Bohuzial, vase skore nie je dostatocne, testom ste nepresli!", "audio"),
+    METHODICAL_LEARNING_TEST_ENTERED("Zacina sa postupovy test", "audio"),
+    METHODICAL_LEARNING_ENDED("Koniec postupoveho testu!", "audio"),
+    METHODICAL_LEARNING_TEST_PASSED("Gratulujem! Uspesne ste presli postupovym testom!", "audio"),
+    METHODICAL_LEARNING_TEST_NOT_PASSED("Bohuzial, vase skore nie je dostatocne, postupovym testom ste nepresli!", "audio"),
 
-    TESTING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre {} a potvrd!", "audio"),
+    TESTING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre pismeno '{}' a potvrd!", "audio"),
     TESTING_SEQUENCE_END("Koniec testu!", "audio"),
     TESTING_SEQUENCE_OVERALL_RESULT("Vysledok testu {}, uspesnost: {}%", "audio"),
 
@@ -84,5 +85,15 @@ public enum Messages implements IMessage {
     @Override
     public String getAudioFile() {
         return audioFile;
+    }
+
+    private static String exportMessages() {
+        EnumSet<Messages> set = EnumSet.allOf(Messages.class);
+        StringBuilder b = new StringBuilder();
+        for (Messages m : set){
+            b.append(String.format("%s|%s", m.name(), m.getText()));
+            b.append("\n");
+        }
+        return b.toString();
     }
 }
