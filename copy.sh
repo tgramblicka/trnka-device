337d20577b4ce75b594f31e6fcb6f3a7678ac3a3
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-03-08 22:15:01 +0100

    copy script from PI to machine

--- copy.sh
+++ copy.sh
@@ -3,6 +3,11 @@
 #mvn clean compile assembly:single
 #mvn clean install
 
+
 #scp ./target/trnka-device.jar pi@192.168.1.15:trnka-device/
+#scp pi@192.168.1.15:/home/pi/audio/* ./
 rsync --progress -avz target/trnka-device.jar pi@192.168.1.15:/home/pi/trnka-device
+
+
+
 
