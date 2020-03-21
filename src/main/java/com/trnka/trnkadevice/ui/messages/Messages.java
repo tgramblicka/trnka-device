ecbe4328d347a635d332cc60eee96a763a125896
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-03-21 20:27:22 +0100

    Message prefixed with directory

--- src/main/java/com/trnka/trnkadevice/ui/messages/Messages.java
+++ src/main/java/com/trnka/trnkadevice/ui/messages/Messages.java
@@ -1,148 +1,148 @@
 package com.trnka.trnkadevice.ui.messages;
 
-import com.trnka.trnkadevice.ui.Renderable;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Messages {
-    AAAA("Ahoj {1,2,3}}", ""),
+    AAAA("Ahoj {1,2,3}}", "commands/"),
 
-    LOGIN_VIEW_LABEL("Prihlasenie", "audio"),
-    TYPE_IN_YOUR_PASSWORD("Zadaj 4 miestny prihlasovaci kod a potvrd", "audio"),
-    YOU_HAVE_BEEN_SUCCESSFULY_LOGGED_IN("Boli ste uspesne prihlaseny", "audio"),
-    WRONG_PASSWORD("Heslo nespravne", "audio"),
-    MAIN_MENU_LABEL("Sekcia: Hlavne menu - Student", "audio"),
-    MAIN_MENU("Vitaj, posun v menu tlacidlami hore/dole. Vstup do menu potvrdzovacim tlacidlom", "audio"),
-    LEARNING_LABEL_MENU("Vyuka", "audio"),
-    LEARNING_VIEW_MENU("Sekcia: Vyuka, vyber moznosti tlacidlami hore/dole", "audio"),
+    LOGIN_VIEW_LABEL("Prihlasenie", "commands/"),
+    TYPE_IN_YOUR_PASSWORD("Zadaj 4 miestny prihlasovaci kod a potvrd", "commands/"),
+    YOU_HAVE_BEEN_SUCCESSFULY_LOGGED_IN("Boli ste uspesne prihlaseny", "commands/"),
+    WRONG_PASSWORD("Heslo nespravne", "commands/"),
+    MAIN_MENU_LABEL("Sekcia: Hlavne menu - Student", "commands/"),
+    MAIN_MENU("Vitaj, posun v menu tlacidlami hore/dole. Vstup do menu potvrdzovacim tlacidlom", "commands/"),
+    LEARNING_LABEL_MENU("Vyuka", "commands/"),
+    LEARNING_VIEW_MENU("Sekcia: Vyuka, vyber moznosti tlacidlami hore/dole", "commands/"),
 
-    TESTING_VIEW("Sekcia: Testovanie pismen. Vyber test tlacidlami hore/dole", "audio"),
+    TESTING_VIEW("Sekcia: Testovanie pismen. Vyber test tlacidlami hore/dole", "commands/"),
 
-    RESULTS_LABEL_MENU("Vysledky", "audio"),
-    RESULTS_SELECTION_VIEW("Sekcia Vysledky: Vyber vysledok testu zo zoznamu tlacidlami hore/dole", "audio"),
-    RESULTS_VIEW("Vysledky pre vybrany test", "audio"),
-    RESULT_NO_RESULTS_TO_DISPLAY("Nenasli sa ziadne Vase vysledky", "audio"),
+    RESULTS_LABEL_MENU("Vysledky", "commands/"),
+    RESULTS_SELECTION_VIEW("Sekcia Vysledky: Vyber vysledok testu zo zoznamu tlacidlami hore/dole", "commands/"),
+    RESULTS_VIEW("Vysledky pre vybrany test", "commands/"),
+    RESULT_NO_RESULTS_TO_DISPLAY("Nenasli sa ziadne Vase vysledky", "commands/"),
 
-    LEARNING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre {} a potvrd! Pismeno pozostava z bodov:", ""),
-    LEARNING_TYPE_IN_CHARACTER_BRAIL_1("Zadaj brail sekvenciu pre {}", ""),
-    LEARNING_TYPE_IN_CHARACTER_BRAIL_2("a potvrd! Pismeno pozostava z bodov:", "audio"),
+    LEARNING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre {} a potvrd! Pismeno pozostava z bodov:", "commands/"),
+    LEARNING_TYPE_IN_CHARACTER_BRAIL_1("Zadaj brail sekvenciu pre {}", "commands/"),
+    LEARNING_TYPE_IN_CHARACTER_BRAIL_2("a potvrd! Pismeno pozostava z bodov:", "commands/"),
 
-    LEARNING_CORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("Spravne, pokracujem na dalsie pismeno", "audio"),
-    LEARNING_INCORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("NESPRAVNE! Správne poradie bodov v znaku je {}", "audio"),
-    LEARNING_INCORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED_LEFT_RETRIES("Ostáva {} pokusov", "audio"),
+    LEARNING_CORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("Spravne, pokracujem na dalsie pismeno", "commands/"),
+    LEARNING_INCORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED("NESPRAVNE! Správne poradie bodov v znaku je {}", "commands/"),
+    LEARNING_INCORRECT_CHARACTER_BRAIL_SEQUENCE_SUBMITTED_LEFT_RETRIES("Ostáva {} pokusov", "commands/"),
 
-    LEARNING_SEQUENCE_END("Koniec výučbového bloku {}. Chceš spustiť postupový test?", "audio"),
+    LEARNING_SEQUENCE_END("Koniec výučbového bloku {}. Chceš spustiť postupový test?", "commands/"),
 
-    TESTING_LETTERS_LABEL_MENU("Testovanie pismen", "audio"),
+    TESTING_LETTERS_LABEL_MENU("Testovanie pismen", "commands/"),
 
-    LEARNING_LETTERS_SELECTION_MENU("Pismena", "audio"),
-    LEARNING_LETTERS_SELECTION_VIEW("Sekcia: Metodicka vyuka - pismena. Vyber vyuku tlacidlami hore/dole", "audio"),
+    LEARNING_LETTERS_SELECTION_MENU("Pismena", "commands/"),
+    LEARNING_LETTERS_SELECTION_VIEW("Sekcia: Metodicka vyuka - pismena. Vyber vyuku tlacidlami hore/dole", "commands/"),
 
-    LEARNING_SYLLABLES_SELECTION_MENU("Slabiky", "audio"),
-    LEARNING_SYLLABLES_SELECTION_VIEW("Sekcia: Metodicka vyuka - slabiky. Vyber moznosti tlacidlami hore/dole", "audio"),
+    LEARNING_SYLLABLES_SELECTION_MENU("Slabiky", "commands/"),
+    LEARNING_SYLLABLES_SELECTION_VIEW("Sekcia: Metodicka vyuka - slabiky. Vyber moznosti tlacidlami hore/dole", "commands/"),
 
-    LEARNING_WORDS_SELECTION_MENU("Slova", "audio"),
-    LEARNING_WORDS_SELECTION_VIEW("Sekcia: Metodicka vyuka - slova. Vyber moznosti tlacidlami hore/dole", "audio"),
+    LEARNING_WORDS_SELECTION_MENU("Slova", "commands/"),
+    LEARNING_WORDS_SELECTION_VIEW("Sekcia: Metodicka vyuka - slova. Vyber moznosti tlacidlami hore/dole", "commands/"),
 
-    LEARNING_SENTENCES_MENU("Vety", "audio"),
-    LEARNING_SENTENCES_VIEW("Sekcia: Metodicka vyuka - vety. Vyber moznosti tlacidlami hore/dole", "audio"),
+    LEARNING_SENTENCES_MENU("Vety", "commands/"),
+    LEARNING_SENTENCES_VIEW("Sekcia: Metodicka vyuka - vety. Vyber moznosti tlacidlami hore/dole", "commands/"),
 
-    NO_SEQUENCES_FOUND("Nenasli sa ziadne vyukove sekvencie!", "audio"),
+    NO_SEQUENCES_FOUND("Nenasli sa ziadne vyukove sekvencie!", "commands/"),
 
-    METHODICAL_LEARNING_MENU("Metodicka vyuka", "audio"),
-    METHODICAL_LEARNING_MENU_VIEW("Sekcia Metodicka vyuka menu: vyber druh vyuky tlacidlami hore/dole", "audio"),
-    METHODICAL_LEARNING_TEST_ENTERED("Zacina sa postupovy test", "audio"),
-    METHODICAL_LEARNING_ENDED("Koniec postupoveho testu!", "audio"),
-    METHODICAL_LEARNING_TEST_PASSED("Gratulujem! Uspesne ste presli postupovym testom!", "audio"),
+    METHODICAL_LEARNING_MENU("Metodicka vyuka", "commands/"),
+    METHODICAL_LEARNING_MENU_VIEW("Sekcia Metodicka vyuka menu: vyber druh vyuky tlacidlami hore/dole", "commands/"),
+    METHODICAL_LEARNING_TEST_ENTERED("Zacina sa postupovy test", "commands/"),
+    METHODICAL_LEARNING_ENDED("Koniec postupoveho testu!", "commands/"),
+    METHODICAL_LEARNING_TEST_PASSED("Gratulujem! Uspesne ste presli postupovym testom!", "commands/"),
     METHODICAL_LEARNING_TEST_NOT_PASSED(
-            "Žial, vase skore nie je dostatocne pre postup do ďalšieho výučbového bloku. Opakujte prosím test alebo sa vráťte späť na výučbu.", "audio"),
+            "Žial, vase skore nie je dostatocne pre postup do ďalšieho výučbového bloku. Opakujte prosím test alebo sa vráťte späť na výučbu.", "commands/"),
 
-    TESTING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre pismeno '{}' a potvrd!", "audio"),
-    TESTING_SEQUENCE_END("Koniec testu!", "audio"),
-    TESTING_SEQUENCE_OVERALL_RESULT("Vysledok testu {}{}{}", "audio"), // ZMENENE
+    TESTING_TYPE_IN_CHARACTER_BRAIL("Zadaj brail sekvenciu pre pismeno '{}' a potvrd!", "commands/"),
+    TESTING_SEQUENCE_END("Koniec testu!", "commands/"),
+    TESTING_SEQUENCE_OVERALL_RESULT("Vysledok testu {}{}{}", "commands/"), // ZMENENE
 
-    STATISTIC_SEQUENCE("Statistika sekvencie", "audio"), // ZMENENY LEN NAZOV
-    STATISTIC_LETTER("Statistika pre pismeno {}", ""),
-    TEST_RESULT_COMPONENT_LABEL("TEST {}", "audio"),
+    STATISTIC_SEQUENCE("Statistika sekvencie", "commands/"), // ZMENENY LEN NAZOV
+    STATISTIC_LETTER("Statistika pre pismeno {}", "commands/"),
+    TEST_RESULT_COMPONENT_LABEL("TEST {}", "commands/"),
 
-    LOG_OUT("Odhlasenie", "audio"),
-    LOGGING_OUT("Pouzivatel bude odhlaseny", "audio"),
+    LOG_OUT("Odhlasenie", "commands/"),
+    LOGGING_OUT("Pouzivatel bude odhlaseny", "commands/"),
 
-    XXX("XXXX, !", "audio"),
+    XXX("XXXX, !", "commands/"),
 
-    CORRECT_GUESS("Spravne", ""), // TREBA PREHRAT !!!!
-    INCORRECT_GUESS("Nespravne", ""),
+    CORRECT_GUESS("Spravne", "commands/"),
+    INCORRECT_GUESS("Nespravne", "commands/"),
 
-    LEARNING_SEQUENCE_NAME("Vyuka {}", "audio"), // parameter bude brail representation
-    TESTING_SEQUENCE_NAME("Testovanie {}", "audio"), // parameter bude brail representation
-    STATISTIC_LETTER_GUESSED("Pismeno {} uhadnute {}", ""),
-    STATISTIC_INCORRECT_RETRIES("Pocet nespravnych pokusov {}{}{}", ""), // NAHRAT
+    LEARNING_SEQUENCE_NAME("Vyuka {}", "commands/"), // parameter bude brail representation
+    TESTING_SEQUENCE_NAME("Testovanie {}", "commands/"), // parameter bude brail representation
+    STATISTIC_LETTER_GUESSED("Pismeno {} uhadnute {}", "commands/"),
+    STATISTIC_INCORRECT_RETRIES("Pocet nespravnych pokusov {}{}{}", "commands/"), // NAHRAT
 
-    ZERO("0", "audio"),   // PRENAHRAT
-    ONE("1", "audio"),
-    TWO("2", "audio"),
-    THREE("3", "audio"),
-    FOUR("4", "audio"),
-    FIVE("5", "audio"),
-    SIX("6", "audio"),
-    SEVEN("7", "audio"),
-    EIGHT("8", "audio"),
-    NINE("9", "audio"),
-    TEN("10", "audio"),
+    ZERO("0", "letters/"),   // PRENAHRAT
+    ONE("1", "letters/"),
+    TWO("2", "letters/"),
+    THREE("3", "letters/"),
+    FOUR("4", "letters/"),
+    FIVE("5", "letters/"),
+    SIX("6", "letters/"),
+    SEVEN("7", "letters/"),
+    EIGHT("8", "letters/"),
+    NINE("9", "letters/"),
+    TEN("10", "letters/"),
 
-    A("a", "letters"),
-    B("b", "letters"),
-    C("c", "letters"),
-    D("d", "audio"),
-    E("e", "audio"),
-    F("f", "audio"),
-    G("g", "audio"),
-    H("h", "audio"),
-    I("i", "audio"),
-    J("j", "audio"),
-    K("k", "audio"),
-    L("l", "audio"),
-    M("m", "audio"),
-    N("n", "audio"),
-    O("o", "audio"),
-    P("p", "audio"),
-    R("r", "audio"),
-    S("s", "audio"),
-    T("t", "audio"),
-    U("u", "audio"),
-    V("v", "audio"),
-    X("x", "audio"),
-    Y("y", "audio"),
-    Z("z", "audio"),
+    A("a", "letters/"),
+    B("b", "letters/"),
+    C("c", "letters/"),
+    D("d", "letters/"),
+    E("e", "letters/"),
+    F("f", "letters/"),
+    G("g", "letters/"),
+    H("h", "letters/"),
+    I("i", "letters/"),
+    J("j", "letters/"),
+    K("k", "letters/"),
+    L("l", "letters/"),
+    M("m", "letters/"),
+    N("n", "letters/"),
+    O("o", "letters/"),
+    P("p", "letters/"),
+    R("r", "letters/"),
+    S("s", "letters/"),
+    T("t", "letters/"),
+    U("u", "letters/"),
+    V("v", "letters/"),
+    W("w", "letters/"),
+    X("x", "letters/"),
+    Y("y", "letters/"),
+    Z("z", "letters/"),
+
 
     // for testing messageToAudioPlayilist compiler
-    TEST0("text", ""),
-    TEST1("{}text", ""),
-    TEST2("text{}", ""),
-    TEST3("{}{}text{}{}", ""),
-    TEST4("{}{}text{}text{}{}", ""),
-    TEST5("{}{}text{}{}text{}{}", ""),
-    TEST6("text{}", ""),
-    TEST7("{}text", ""),
-    TEST8("text{}text", ""),
-    TEST9("text{}{}text", ""),
+    TEST0("text", "commands/"),
+    TEST1("{}text", "commands/"),
+    TEST2("text{}", "commands/"),
+    TEST3("{}{}text{}{}", "commands/"),
+    TEST4("{}{}text{}text{}{}", "commands/"),
+    TEST5("{}{}text{}{}text{}{}", "commands/"),
+    TEST6("text{}", "commands/"),
+    TEST7("{}text", "commands/"),
+    TEST8("text{}text", "commands/"),
+    TEST9("text{}{}text", "commands/"),
 
 
-    LEARNING_SEQUENCE_STARTED("Zacali ste sekvenciu ucenia cislo {}", "audio"),
-    UNKNOWN("Unknown", "");
+    LEARNING_SEQUENCE_STARTED("Zacali ste sekvenciu ucenia cislo {}", "commands/"),
+    UNKNOWN("Unknown", "commands/");
 
     private final String text;
-    private final String audioFile;
+    private final String directory;
 
     Messages(final String text,
-             final String audioFile) {
+             final String directory) {
         this.text = text;
-        this.audioFile = audioFile;
+        this.directory = directory;
     }
 
     public String getText() {
@@ -150,11 +150,11 @@ public enum Messages {
     }
 
     public String getAudioFile() {
-        return String.format("%s.wav", name());
+        return String.format("%s%s.wav", directory, name());
     }
 
     public String getNthPartOfAudioFile(Integer n) {
-        return String.format("%s_%s.wav", name(), n);
+        return String.format("%s%s_%s.wav", directory, name(), n);
     }
 
     private static String exportMessages() {
