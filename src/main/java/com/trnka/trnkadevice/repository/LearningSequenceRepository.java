573156d7202199f1a628bbbd1a544dbc271c0300
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-02-17 23:04:58 +0100

    IndividualLearningSequences fetched by username & adapted query & removed unused LearningSequenceDao

--- src/main/java/com/trnka/trnkadevice/repository/LearningSequenceRepository.java
+++ src/main/java/com/trnka/trnkadevice/repository/LearningSequenceRepository.java
@@ -1,9 +1,17 @@
 package com.trnka.trnkadevice.repository;
 
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.trnka.trnkadevice.domain.LearningSequence;
 
 public interface LearningSequenceRepository extends CrudRepository<LearningSequence, Long> {
+
+    @Query(value = "SELECT userSeq from User usr JOIN usr.sequences AS userSeq WHERE usr.username = :username AND type(userSeq) IN ('LS')")
+    Set<LearningSequence> findAllLearningSequencesOfUser(@Param("username") String username);
+
 
 }
--- src/main/java/com/trnka/trnkadevice/ui/learning/IndividualLearningMenuView.java
+++ src/main/java/com/trnka/trnkadevice/ui/learning/IndividualLearningMenuView.java
@@ -5,54 +5,41 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.trnka.trnkadevice.domain.Sequence;
-import com.trnka.trnkadevice.ui.MenuStudentView;
-import com.trnka.trnkadevice.ui.SequenceComponent;
-import com.trnka.trnkadevice.ui.messages.AudioMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.trnka.trnkadevice.dao.LearningSequenceDAO;
 import com.trnka.trnkadevice.domain.LearningSequence;
+import com.trnka.trnkadevice.domain.Sequence;
 import com.trnka.trnkadevice.renderer.IRenderer;
+import com.trnka.trnkadevice.repository.LearningSequenceRepository;
 import com.trnka.trnkadevice.ui.CycledComponent;
 import com.trnka.trnkadevice.ui.IView;
+import com.trnka.trnkadevice.ui.MenuStudentView;
+import com.trnka.trnkadevice.ui.SequenceComponent;
 import com.trnka.trnkadevice.ui.UserSession;
+import com.trnka.trnkadevice.ui.messages.AudioMessage;
 import com.trnka.trnkadevice.ui.messages.Messages;
 import com.trnka.trnkadevice.ui.navigation.Navigator;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@RequiredArgsConstructor
 public class IndividualLearningMenuView implements IView {
 
-    private Navigator navigator;
-    private IRenderer renderer;
-    private LearningSequenceDAO learningSequenceDAO;
-    private UserSession userSession;
-    private CycledComponent cycledComponent;
-    private IndividualLearningView individualLearningView;
-
-    @Autowired
-    public IndividualLearningMenuView(final Navigator navigator,
-                                      final IRenderer renderer,
-                                      final LearningSequenceDAO learningSequenceDAO,
-                                      final UserSession userSession,
-                                      final CycledComponent cycledComponent,
-                                      final IndividualLearningView individualLearningView) {
-        this.navigator = navigator;
-        this.renderer = renderer;
-        this.learningSequenceDAO = learningSequenceDAO;
-        this.userSession = userSession;
-        this.cycledComponent = cycledComponent;
-        this.individualLearningView = individualLearningView;
-    }
+    private final Navigator navigator;
+    private final IRenderer renderer;
+    private final LearningSequenceRepository learningSequenceRepository;
+    private final UserSession userSession;
+    private final CycledComponent cycledComponent;
+    private final IndividualLearningView individualLearningView;
 
     @Override
     public void enter() {
         renderer.renderMessage(AudioMessage.of(Messages.LEARNING_VIEW_MENU));
-        Set<LearningSequence> sequences = learningSequenceDAO.getLearningSequences(userSession.getUsername());
+        Set<LearningSequence> sequences = learningSequenceRepository.findAllLearningSequencesOfUser(userSession.getUsername());
 
         List<SequenceComponent> selection = sequences.stream().map(SequenceComponent::new).collect(Collectors.toList());
         cycledComponent.cycleThroughComponents(index -> startLearningWithSequence(selection.get(index).getSequence()), selection);
