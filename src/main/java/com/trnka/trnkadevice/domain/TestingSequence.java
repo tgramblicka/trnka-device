4fd7dc8f086293c05b6c3082cd5a927d3b943312
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-05-07 00:12:44 +0200

    TestinSequence has a passing_rate also

--- src/main/java/com/trnka/trnkadevice/domain/TestingSequence.java
+++ src/main/java/com/trnka/trnkadevice/domain/TestingSequence.java
@@ -1,13 +1,39 @@
 package com.trnka.trnkadevice.domain;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+import org.hibernate.validator.constraints.Range;
+
+import com.trnka.trnkadevice.domain.visitor.SequenceVisitor;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @DiscriminatorValue(value = "TS")
+@Getter
+@Setter
+@EqualsAndHashCode
 public class TestingSequence extends Sequence {
 
     public TestingSequence() {
         super();
+    }
+
+    /**
+     * In % 0-100
+     */
+    @Range(min = 0, max = 100)
+    @Column(name = "passing_rate_percentage")
+    private BigDecimal passingRate;
+
+    @Override
+    public <T> T accept(final SequenceVisitor<T> visitor) {
+        return visitor.visit(this);
     }
 }
