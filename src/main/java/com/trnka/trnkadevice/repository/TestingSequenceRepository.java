5e9e2083b8f6eebe7d193f4fcb9c44a47d6400ee
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-05-07 00:13:40 +0200

    fix of a findAllTestingSequencesForUser - must return List instead of set

--- src/main/java/com/trnka/trnkadevice/repository/TestingSequenceRepository.java
+++ src/main/java/com/trnka/trnkadevice/repository/TestingSequenceRepository.java
@@ -1,5 +1,6 @@
 package com.trnka.trnkadevice.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -12,7 +13,7 @@ import com.trnka.trnkadevice.domain.TestingSequence;
 public interface TestingSequenceRepository extends CrudRepository<TestingSequence, Long> {
 
     @Query(value = "SELECT seq from UserSequence AS usrSeq JOIN usrSeq.sequence AS seq WHERE usrSeq.user.username = :username AND type(seq) IN ('TS')")
-    Set<TestingSequence> findAllTestingSequencesForUser(@Param("username") String username);
+    List<TestingSequence> findAllTestingSequencesForUser(@Param("username") String username);
 
     Optional<TestingSequence> findByExternalId(final Long id);
 
