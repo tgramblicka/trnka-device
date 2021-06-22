c3c289532c4edd2351f25fa3f1583f78bd51d3ab
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-06-22 22:19:54 +0200

    run.sh - adapted when need to run with JMX port opened

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,3 +19,13 @@ logFile="/home/pi/trnka-device/logs/log-`date '+%Y-%m-%d-%H:%M:%S'`.log"
 touch $logFile
 #nohup java -DMARIA_DB_USER=pi -DMARIA_DB_PWD=raspberry -DTRNKA_SOUNDS_LOCATION=/home/pi/trnka-device/sounds/ -jar /home/pi/trnka-device/trnka-device.jar >> $logFile 2>&1 &
 nohup java -jar /home/pi/trnka-device/trnka-device.jar >> $logFile 2>&1 &
+
+# uncomment below if need to run with jmx enabled
+#nohup java -Dcom.sun.management.jmxremote \
+#-Dcom.sun.management.jmxremote.port=9010 \
+#-Dcom.sun.management.jmxremote.rmi.port=9010 \
+#-Dcom.sun.management.jmxremote.local.only=false \
+#-Dcom.sun.management.jmxremote.authenticate=false \
+#-Dcom.sun.management.jmxremote.ssl=false \
+#-Djava.rmi.server.hostname=192.168.0.107 \
+#-jar /home/pi/trnka-device/trnka-device.jar >> $logFile 2>&1 &
